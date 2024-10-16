c733e4a2d268a30cc00f77882ce52724f5664b46
VIP BISHNOI <154018946+VIPHACKE@users.noreply.github.com>
2024-10-17 00:55:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+function searchVideos() {
+    const query = document.getElementById('search').value;
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=YOUR_API_KEY&type=video`)
+        .then(response => response.json())
+        .then(data => {
+            const videoList = document.getElementById('video-list');
+            videoList.innerHTML = '';
+            data.items.forEach(video => {
+                const videoElement = document.createElement('div');
+                videoElement.classList.add('video');
+                videoElement.innerHTML = `
+                    <img src="${video.snippet.thumbnails.medium.url}" alt="${video.snippet.title}">
+                    <h3>${video.snippet.title}</h3>
+                `;
+                videoList.appendChild(videoElement);
+            });
+        });
+}
